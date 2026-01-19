a0df8d5caf4112621399d38719fdd4b873e8e5fd
Vero2583 <vfraigneau64@gmail.Com>
2026-01-19 14:02:40 +0100

    gerer connexion base de données

---
+++ backend/src/config/db.js
@@ -0,0 +1,29 @@
+import mysql from 'mysql2/promise'
+import 'dotenv/config'
+
+
+
+let db;
+let env = process.env
+
+try {
+    
+    db = await mysql.createPool({
+        host: env.DB_HOST,
+        user: env.DB_USER,
+        database : env.DB_NAME
+
+    })
+
+    await db.getConnection(
+        console.log(`connexion à la base de données ${process.env.DB_NAME} est réussie`)
+    )
+
+} catch (error) {
+    console.error(`erreur lors de la connexion à la base de données `, error.message )
+    process.exit(1)
+    
+}
+
+export default db
+
